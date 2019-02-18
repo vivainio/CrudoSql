172ce163cf7c17e35055e5f0ebadd24509a864b8
Ville M. Vainio <ville.vainio@basware.com>
2019-02-18 14:19:36 +0200

    add source files

---
+++ CrudoSql/Db.fs
@@ -0,0 +1,280 @@
+﻿module Crudo.Db
+
+open SqlFrags.SqlGen
+open System
+open System.Data
+
+
+module DbConnector =
+    open System.Diagnostics
+
+    let MsSql = "" // typeof<System.Data.SqlClient.SqlConnection>.AssemblyQualifiedName
+    let OleDb = "" // typeof<System.Data.OleDb.OleDbConnection>.AssemblyQualifiedName
+    // let ODP = typeof<Oracle.ManagedDataAccess.Client.OracleConnection>.AssemblyQualifiedName
+
+    [<DebuggerDisplay("{provider} -> {connectionString}")>] 
+    type Connector(provider: string, connectionString: string) = 
+        let createConnection() = 
+            let typ = Type.GetType(provider)
+            System.Activator.CreateInstance(typ, connectionString) :?> IDbConnection
+                
+        let lazyConnection = lazy(createConnection())
+
+        // reuse same dbconnection instance.
+        member x.Reuse() =
+            let conn = lazyConnection.Value
+            if conn.State <> ConnectionState.Open then conn.Open()
+            conn
+        
+        // create new connection. You need to call conn.Open() yourself
+        member x.Connect() = 
+            let conn = createConnection()
+            conn.Open()
+            conn
+
+        member x.Show() = sprintf "%s :: %s" (provider.Split(',').[0]) connectionString
+        member x.Syntax() = if provider.Contains "SqlClient" then SqlSyntax.Any else SqlSyntax.Ora
+
+    type ConnectionStringFrag = 
+    | Provider of string
+    | ProviderAccess
+    | DataSource of string
+    | Catalog of string
+    | UserId of string
+    | Password of string
+    | IntegratedSecurity of bool
+   
+    with
+        member x.Str = match x with
+                       | Provider s -> "Provider=" + s
+                       | DataSource s -> "Data Source=" + s
+                       | Catalog s -> "Initial Catalog=" + s
+                       | IntegratedSecurity true -> "Integrated Security=True"
+                       | IntegratedSecurity false -> "Integrated Security=no"
+                       | ProviderAccess -> "Provider=Microsoft.ACE.OLEDB.12.0"
+                       | Password s -> "Password="+s
+                       | UserId s -> "User Id=" + s
+
+    let ConnectionString (parts: ConnectionStringFrag seq) = 
+        parts |> Seq.map (fun f -> f.Str) |> String.concat ";"
+
+    let OracleConnectionString host port service username password =
+        sprintf "Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=%s)(PORT=%d))(CONNECT_DATA=(SERVICE_NAME=%s)));User Id=%s;Password=%s;" host port service username password
+        
+    let mutable DefaultConnector = 
+        Connector(MsSql, ConnectionString [ DataSource "localhost"; Catalog "IA"; IntegratedSecurity true])
+ 
+
+open DbConnector
+
+let ConnectToDefault() =
+    let connector = DefaultConnector
+    connector.Connect()
+
+// data as read from raw db query
+type RawTableRec = {
+    Name: string
+    Header: string[]
+    Rows: obj [] []
+}
+[<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
+module RawTableRec = 
+
+    let FilterCols (cols: string[]) (table: RawTableRec) =
+        
+        let indices = table.Header |> Array.mapi (fun i h -> if Array.contains h cols then Some(i) else None) |> Array.choose id
+        
+        seq {
+            for row in table.Rows do
+                yield (indices |> Array.map (row |> Array.get))
+        }
+    
+    let ChooseCols f (table: RawTableRec) = 
+        seq {
+            for row in table.Rows do
+
+                let chosen = 
+                    row 
+                    |> Array.mapi (fun i col -> (row, table.Header.[i], col))
+                    |> Array.choose f
+                yield chosen
+        }
+
+    let KeyValPairs (trec: RawTableRec) =
+        seq {
+            for row in trec.Rows do
+                yield (trec.Header |> Array.mapi (fun i hname -> (hname, row.[i])))
+        } |> Array.ofSeq
+    let AsMaps trec = 
+        let pairs = KeyValPairs trec
+        seq {
+            for row in pairs do
+                yield Map.ofSeq row
+        } |> Array.ofSeq
+        
+
+type TableCell = 
+| Any of obj
+| Link of string*string // href, label
+with member x.Val = match x with 
+                    | Any o -> o.ToString()
+                    | Link(_, label) -> label
+
+type ForeignKey = {
+    Target: ColRef     
+    Summarizer: ColRef
+}
+
+type TableHint = 
+| SortCol of string
+| ValueFilter of string*(string->string)
+| FillAll
+
+type DeferFunc = TableSpec -> ColSpec -> ColSpec option
+and ColSpec =
+| Prim of string
+| Col of string
+| Fk of string*ForeignKey
+| Summ of string
+| Defer of ColSpec*DeferFunc
+with
+    member x.Name =
+        match x with
+        | Prim s -> s
+        | Col s -> s
+        | Summ s -> s
+        | Fk (col,fk) -> col
+        | Defer (cs, _) -> cs.Name
+
+and TableSpec() =
+    member val Name = "" with get,set
+    member val Cols: ColSpec[] = [||] with get,set
+    member val Hints = ResizeArray<TableHint>() with get,set
+with
+    
+    member x.Prim = x.Cols |> Array.pick (function | Prim n -> Some n | _ -> None)
+    member x.Summ = x.Cols |> Array.tryPick (function | Summ n -> Some n | _ -> None) |> Option.defaultValue x.Cols.[0].Name
+    member x.ForeignKeys = x.Cols |> Array.filter (function | Fk _ -> true | _ -> false)
+    member x.GetCol name = x.Cols |> Array.find (fun col -> col.Name = name)
+    member x.TryGetCol name = x.Cols |> Array.tryFind (fun col -> col.Name = name)
+
+    member x.Ref = Table x.Name
+     
+
+type DbSpec() = 
+    member val Tables = ResizeArray<TableSpec>() with get, set
+    member x.TryFindTable name = x.Tables |> Seq.tryFind (fun t -> t.Name = name)
+    member x.ReferringForeignKeys (toTable: Table) = 
+        x.Tables 
+        |> Seq.choose (fun t ->
+            let fksToHere = t.ForeignKeys
+                            |> Array.filter (function
+                                          | Fk(_, fk) when fk.Target.Table = toTable.Name -> true
+                                          | _ -> false)
+            if Array.isEmpty fksToHere then None else Some (t, fksToHere)
+            )
+        |> Seq.toArray
+          
+let readToArr (r: IDataReader) =
+    [| 0..(r.FieldCount-1) |] |> Array.map (fun i -> r.GetValue i)
+
+type Conn() =
+    let conn = ConnectToDefault()
+
+    member x.Syntax = 
+        DefaultConnector.Syntax()
+    
+    member x.Connection = conn
+
+    member x.Sql q = serializeSql x.Syntax q
+
+    // not really useful with mssql
+    member x.WithObjs sql (values: obj seq) =
+        let q = conn.CreateCommand()
+        q.CommandText <- sql
+        for v in values do
+            
+            let p = q.CreateParameter()
+            p.Value <- v
+            q.Parameters.Add(p) |> ignore
+        x.QueryImpl q
+
+    member x.WithParams sql (values: (string*obj) seq) =
+        let q = conn.CreateCommand()
+        q.CommandText <- sql
+        for (k,v) in values do
+            let p = q.CreateParameter()
+            p.ParameterName <- k
+            p.Value <- v
+            q.Parameters.Add(p) |> ignore
+        x.QueryImpl q
+
+    member x.Query tableName sql =
+        let q = conn.CreateCommand()
+        q.CommandText <- sql;
+        x.QueryImpl q
+        
+
+    // tableName is optional informational argument, set to "" if don't care
+    member x.QueryImpl q =
+        use reader = q.ExecuteReader()
+        let headers = [| 0..(reader.FieldCount-1) |] |> Array.map (fun i -> reader.GetName i)
+        let valArray = Array.zeroCreate reader.FieldCount
+        {
+            Name = "tablename"
+            Header = headers;
+            Rows = 
+                seq {
+                    while reader.Read() do
+                        let valArray = Array.zeroCreate reader.FieldCount
+                        reader.GetValues(valArray) |> ignore
+                        yield valArray
+                } |> Seq.toArray
+        }
+
+
+
+let RunSql (query: Frag seq) =
+    let conn = Conn()
+    query |> serializeSql SqlSyntax.Any |> conn.Query "" |> fun resp -> (resp.Rows, resp)
+
+let cast2<'a, 'b> (objs: obj[]) =
+    (objs.[0] :?> 'a, objs.[1] :?> 'b)
+let cast3<'a, 'b, 'c> (objs: obj[]) =
+    (objs.[0] :?> 'a, objs.[1] :?> 'b, objs.[2] :?> 'c)
+
+
+let Test() =
+    let db = Conn()
+    let q = [
+                SelectS ["*"]
+                FromS ["INFORMATION_SCHEMA.TABLE_CONSTRAINTS Tab"; 
+                 "INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE Col"]
+                WhereS "Col.Constraint_Name = Tab.Constraint_Name"
+
+            ] |> serializeSql SqlSyntax.Any
+    let res = db.Query "" q
+    let testq q = 
+        q |> serializeSql SqlSyntax.Any |> fun s -> printfn "%s" s; s |>  db.Query "" |> printfn "%A"
+
+    let User = Table "ADM_USER_DATA"
+    testq [
+            SelectS ["count(1)"]
+            Raw "from"
+            NestAs("Users",[
+                            SelectS ["*"]
+                            From User
+            ])
+        
+          ]
+    testq [
+            SelectS ["*"]
+            From (Table "PE_PROCESS_TASK")
+            WhereS "recipient_id in "
+            Nest [
+                Select [User.Col("ID")] 
+                From User
+            ]
+           ]
+
+    res
---
+++ CrudoSql/View.fs
@@ -0,0 +1,393 @@
+﻿
+module Crudo.View
+
+open Suave.Html
+open Db
+open SqlFrags.SqlGen
+open Suave
+open System.Net
+open Microsoft.FSharpLu.Json
+
+let safeText txt = System.Net.WebUtility.HtmlEncode(txt) |> text
+let divId id = div ["id", id]
+let divc cl = div ["class", cl]
+let divStyle st = div ["style", st]
+
+let table = tag "table" ["class", "table table-bordered"]
+let tr = tag "tr" []
+let td = tag "td" []
+let th t = (tag "th") [] (text t)
+let textLink (href: string) (title: string) = (tag "a") [ "href", href] (text title)
+let textLinkc (href: string) (title: string) (klass:string) = (tag "a") [ "href", href; "class",klass] (text title)
+
+let buttonLink btnclass (href: string) (title: string)  = (tag "a") [ "href", href; "class","btn " + btnclass; "role","button"] (text title)
+
+let span = tag "span" []
+let ul = tag "ul" []
+let li = tag "li" []
+let textH txt = tag "h3" [] (text txt)
+let fontAwesome (iconName: string) = tag "i" ["class", "fa fa-"+iconName; "aria-hidden", "true"] []
+let awesomeUrl iconName href  = (tag "a") [ "href", href ] [fontAwesome iconName]
+let tagc tname klass = tag tname ["class", klass]
+let htmlEnc s = WebUtility.HtmlEncode s
+
+let importJsModule modName startFunc =
+    script [] <| rawText (sprintf """
+        document.addEventListener("DOMContentLoaded",
+            () => {
+                require(['%s'], m => m.%s())
+            })""" modName startFunc)
+
+
+let MAX_PAGE_SIZE = 2000
+
+type TableReadResults = {
+    Data: RawTableRec
+    TotalCount: int
+    TableName: string
+}
+
+
+type FilterValue =
+| Eq of string*string
+| Meta of string*string
+with
+    member x.Key = match x with | Eq(k,_) | Meta(k,_) -> k
+    member x.Value = match x with | Eq(_,v) | Meta(_,v) -> v
+
+module Filters =
+    let ToUrlQuery (fvals: FilterValue seq) =
+        fvals
+        |> Seq.map (function | Eq(k,v) | Meta(k,v) -> sprintf "%s=%s" k v)
+        |> String.concat "&"
+    let  ToSqlWhereCondition (tableName: string) (fvals: FilterValue[]) =
+        fvals
+        |> Seq.choose (function | Eq(k,v) -> sprintf "%s.%s='%s'" tableName k v |> Some  | _ -> None)
+        |> String.concat " AND "
+        |> fun s -> if s = "" then Skip else WhereS s
+
+    let ParseFromRequest (req: HttpRequest) =
+        let filters = req.query
+                      |> List.choose (function
+                                      | (key, Some value) when key.StartsWith("$") -> Meta(key, value) |> Some
+                                      | (key, Some value) -> Eq(key, value) |> Some
+                                      | _ -> None)
+        Array.ofList filters
+    let TryFind key (filters: FilterValue[]) =
+        filters |> Array.tryFind (fun f -> f.Key = key)
+    let GetPage (offset:int) (batchSize:int) =
+        [| Meta("$skip", string offset); Meta("$limit", string batchSize) |]
+    let WithoutPage(filters: FilterValue[]) =
+        filters |> Array.filter (function | Meta("$skip", _) | Meta("$limit", _) -> false | _ -> true)
+
+module LinkGen =
+    let TableSearch (Table table) col value = sprintf "/table/%s?%s=%s" table col value
+    let TableRaw (Table table) = sprintf "/rawtable/%s" table
+    let TableTop (Table table) = sprintf "/table/%s" table
+    let SearchWithFilters searchType (Table table) filters = sprintf "/%s/%s?%s" searchType table (Filters.ToUrlQuery(filters))
+
+type ViewContext = {
+    Filters: FilterValue[]
+    Db: DbSpec
+    Req: HttpRequest
+}
+
+let mapPairs f =
+    Seq.map(fun t -> f (fst t) (snd t))
+
+let CreatePager totalCount pageSize currentSkip hrefGenerator =
+    let fullPageCount = totalCount / pageSize
+
+    let strides = [| for i in 1 .. fullPageCount -> ((i-1) * pageSize, (i*pageSize-1)) |]
+    let lastPage = [| (fullPageCount*pageSize, totalCount) |]
+    [strides; lastPage]
+    |> Array.concat
+    |> mapPairs (fun st en ->
+        (textLink (hrefGenerator st en) (sprintf "%d - %d" st en)), st = currentSkip)
+    |> Seq.map ( fun (nod, isActive) -> tag "li" (if isActive then ["class","active"] else []) [nod])
+    |> List.ofSeq
+    |> tag "ul" ["class","pagination"]
+
+let renderPage content =
+    let loadCss url =
+            tag "link" [
+                "href", url
+                "rel", "stylesheet"
+                ] []
+
+    let loadJs url =
+        script ["src", "/assets/js/" + url] []
+    let navLink href txt = tag "li" [] [tag "a" ["class", "nav-link"; "href", href] (text txt)]
+
+    html ["lang", "en"] [
+        head [] [
+            title [] "CrudoSql"
+            loadCss "https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+
+            tag "meta" ["http-equiv", "Content-Language"; "content", "en"] []
+        ]
+
+        body [] [
+            tagc "nav" "navbar navbar-default" [
+                tagc "ul" "nav navbar-nav" [
+                    navLink "/" "Home"
+                    navLink "/alltables/" "All"
+                    navLink "/meta/" "Metadata"
+                    navLink "/log/" "SQL log"
+                ]
+            ]
+
+            divc "container-fluid" [
+                content
+            ]
+        ]
+    ]
+    |> htmlToString
+
+let tableView headingNode allTools tableNode filters preamble (readResults: TableReadResults) =
+
+    let filterNode = if Seq.isEmpty filters then [] else (text (Filters.ToUrlQuery filters))
+
+    let full = divId "tableview" ([
+                                    tag "h3" [] [textLink (LinkGen.TableTop (Table readResults.TableName)) readResults.TableName]
+                                    divc "btn-group pull-right" allTools
+                                    tag "em" [] [divc "filter" [span filterNode]]
+                                  ] @ preamble @
+                                    [
+                                    divStyle "clear: both" []
+                                    tableNode
+                                    ])
+
+    full
+
+
+// should not be needed
+let TableRaw (t: RawTableRec) tableName filters (readResult: TableReadResults) =
+    let heading = t.Header |> Seq.map th |> List.ofSeq
+    let toCell (o: obj) = o.ToString() |> text
+
+    let singleElementTable (keys: string[]) (values: obj[]) =
+        let pairs = Array.zip keys values
+        let trs = pairs |> Array.map (fun (k,v) -> tr [td (text k);td (text (v.ToString()))]) |> List.ofArray
+        table trs
+
+
+    let (tableElement, rowtools) =
+        match t.Rows.Length with
+        | 1 ->
+            let singleColTools = [buttonLink "btn-primary" (LinkGen.SearchWithFilters "editrow" (Table tableName) filters) "Edit"]
+            (singleElementTable t.Header t.Rows.[0], singleColTools)
+
+        | _ ->
+            let rows = t.Rows |> Seq.map (fun rowData ->
+                let cells = rowData |> Seq.map (fun o -> toCell o |> td) |> List.ofSeq
+                tr cells) |> List.ofSeq
+
+            let tableRows = [ tr heading ] @ rows
+            (table tableRows, [])
+
+
+    //let allTools = [ buttonLink "btn-primary" (LinkGen.TableTop (Table tableName)) "Unraw" ]
+    let allTools = rowtools @ [ buttonLink "btn-primary" (LinkGen.SearchWithFilters "table" (Table tableName) filters) "Unraw" ]
+
+    tableView (textH tableName) allTools tableElement filters [] readResult |> renderPage
+
+let cellToHtml (c: TableCell) =
+    match c with
+    | TableCell.Any(o) -> span (o.ToString() |> text)
+    | Link(link, label) ->
+        textLink link label
+
+let TableRich (cols: ColSpec[]) (tab: TableCell[][]) (spec: TableSpec) (dbSpec: DbSpec) filters (readResult: TableReadResults) =
+    let colNames = cols |> Seq.map (fun col -> col.Name) |> List.ofSeq
+    let heading = colNames |> Seq.map th |> List.ofSeq
+    let rowTds (rowData: TableCell[]) =
+        rowData |> Array.map (fun c -> [cellToHtml c] |> td) |> List.ofArray
+
+    let rows = tab
+               |> Array.map rowTds
+               //|> Array.truncate 1000
+
+    // got tds, now create trs maybe
+
+    let columnValue (colSpecs: ColSpec[]) (row: TableCell[]) colName =
+        colSpecs |> Array.findIndex (fun c -> c.Name = colName) |> Array.get row |> fun c -> c.Val
+
+
+    let (tableNode, toolLinks, tableLinks) =
+        match rows.Length with
+        | 1 ->  // table with 1 row provides extra features
+            let tdList = rows.[0]
+            let pairs = tdList |> List.zip colNames |> Array.ofList
+            let keyValAsTr(key: string,value) = tr [
+                                                    td (text key)
+                                                    value
+                                                   ]
+            let trLines = pairs |> Array.map keyValAsTr |> List.ofArray
+
+
+            let foreignKeysToLink = dbSpec.ReferringForeignKeys (Table spec.Name)
+                                    |> Array.collect (fun (t, fks) -> fks |> Array.map (fun fk -> (t, fk)))
+                                    |> Array.sortBy (fun (ts,cs) -> cs.Name)
+            let links =
+                seq {
+                    // exist to redece repetition
+                    let mutable previousColValue = ""
+                    for (foreignTable, fk) in foreignKeysToLink do
+                        let colInOtherTable = fk.Name
+                        let colInThisTable = spec.Prim
+                        let valueIntThisTable = columnValue cols tab.[0] colInThisTable
+                        let marker = if previousColValue = colInOtherTable then "" else " (" + colInOtherTable + ")"
+                        previousColValue <- colInOtherTable
+                        let linkhref = LinkGen.SearchWithFilters
+                                        "table"
+                                        (Table foreignTable.Name)
+                                        [Eq(colInOtherTable,valueIntThisTable)]
+
+                        yield divc "tablelink" [
+
+                            (fontAwesome "link")
+                            (textLink linkhref foreignTable.Name)
+                            text marker |> span
+                        ]
+
+
+                } |> Seq.toList
+            let singleColTools = [buttonLink "btn-primary" (LinkGen.SearchWithFilters "editrow" (Table spec.Name) filters) "Edit"]
+            (table trLines, singleColTools, links)
+        | _ ->
+            let nodesUnderTr (nodes: Node list) =  tr nodes
+            let trLines = rows |> Array.map nodesUnderTr |> List.ofArray
+            let tableRows = [ tr heading ] @ trLines
+
+            let noPaging = Filters.WithoutPage filters
+            let makeHref start end_ = LinkGen.SearchWithFilters "table" (Table spec.Name) (Array.concat [noPaging;Filters.GetPage start (end_-start)])
+            let currentSkip = filters |> Filters.TryFind "$skip" |> Option.defaultValue (Meta("$skip","0")) |> fun f -> int f.Value
+            let pager = CreatePager readResult.TotalCount MAX_PAGE_SIZE currentSkip makeHref
+
+            (table tableRows, [], [pager])
+
+    let allTools = toolLinks @ [
+                                buttonLink "btn-primary" (LinkGen.SearchWithFilters "rawtable" (Table spec.Name) filters) "Raw"
+                               ]
+
+    tableView (textH (sprintf "%s (%d)" spec.Name readResult.TotalCount )) allTools tableNode filters tableLinks readResult |> renderPage
+
+
+let emitPageData (id: string) (cont: string) =
+    let embedder =
+        script [
+            "id", id
+            "type","application/json"
+        ] (text cont)
+    embedder
+
+type TableEditDto = {
+    Fields: obj
+    Spec: TableSpec option
+    Filters: FilterValue[]
+}
+
+
+let TableEdit (ctx: ViewContext) (readResult: TableReadResults) =
+    let data = readResult.Data
+    let row = data.Rows.[0]
+    let pairs = Array.zip data.Header data.Rows.[0]
+    let filters = ctx.Filters
+    let tableName = readResult.TableName
+
+    let getFormInputLine key value =
+
+        let label = tag "label" ["for", key] (text key)
+        let inputText initialval = tag "input" ["type", "text";  "name", key; "value", value |> htmlEnc ;
+                                   "class", "form-control";
+                                   "id", key] []
+        divc "form-group" [ label ; inputText value ]
+
+
+    let trs = pairs |> Array.map (fun (k,v) -> getFormInputLine k (v.ToString())) |> List.ofArray
+
+    let submitBtn submitval txt =
+        tag "button" ["type", "submit"; "name","action"; "value", submitval; "class", "btn btn-primary"] (text txt)
+
+
+    let saveSubmit = submitBtn "update" "Save"
+    let insertSubmit = submitBtn "insert" "Insert"
+
+    let tableNode =
+        tag "form" [
+            "action",(LinkGen.SearchWithFilters "saverow" (Table tableName) filters)
+            "method", "post"
+            ] (trs @ [saveSubmit; insertSubmit])
+
+
+    let editTools = [buttonLink "btn-primary" (LinkGen.SearchWithFilters "table" (Table tableName) filters) "Cancel"]
+    let tv = tableView (textH tableName) editTools tableNode filters [] readResult
+    let scripts = importJsModule "app" "startEditRow"
+    (*
+    let testChosen = tag "select" [
+                                    "data-placeholder", "villen placeholder"; "class", "chosen-select"] [
+                                     tag "option" ["value","testval"] (text "hallo")
+                    ]
+    *)
+    let pageDataHolder = emitPageData "pageDataEditRow" "PAGE_DATA_HERE"
+    let pageData = {
+        Fields = (Map.ofSeq pairs) :> obj
+        Spec = ctx.Db.TryFindTable tableName
+        Filters = ctx.Filters
+    }
+    divc "crudo-row-edit" [tv ; scripts; pageDataHolder]
+    |> renderPage
+    |> String.replace "PAGE_DATA_HERE" (Compact.serialize pageData)
+
+let Index(dbSpec: DbSpec, connectionDesc) =
+    let tables = dbSpec.Tables |> Seq.map (fun t -> t.Name)
+    let tablelinks = tables
+                     |> Seq.map ( fun tname ->
+                        tagc "li" "list-group-item" [textLink (LinkGen.TableTop (Table tname)) tname])
+                     |> List.ofSeq
+    divc "crudo-index" [
+        divc "jumbotron" [
+            tag "h1" ["class", "display-3"] <| text "CrudoSql"
+            tag "p" ["class","lead"] <| text "Your schema, your rules. Made with <3 using F# and Suave."
+        ]
+        divc "tablelinks" [tagc "ul" "list-group" tablelinks]
+        tagc "p" "text-right" <| text connectionDesc
+
+    ]
+
+    |> renderPage
+
+let TableList (tables: string[]) =
+    divc "tablist" (
+        tables
+        |> Array.map (fun tname -> (tname, LinkGen.TableTop (Table tname)))
+        |> Array.map (fun (tname, href) -> (tag "a") ["href", href] (text tname))
+        |> Array.map (fun an -> li [an])
+        |> List.ofArray
+    ) |> renderPage
+
+
+let unpack2 els = (Seq.head els, Seq.tail els |> Seq.head)
+
+let MetaData (md: Map<string, seq<seq<string>>>) =
+    md
+    |> Map.toSeq
+    |> Seq.map (fun (k,v) ->
+        divc "md-entry" [
+            (tag "h4") [] [textLink (LinkGen.TableTop (Table k)) k]
+            v |> Seq.map (unpack2 >> (fun (colname, coltype) -> tr [td (text colname); td (text coltype)])) |> List.ofSeq |> table
+        ])
+
+    |> List.ofSeq
+    |> divc "meta"
+    |> renderPage
+
+let SqlLog (ents: seq<string*string>) =
+    let pres = ents |> Seq.map (fun (comment, q) ->
+        [
+            tagc "h3" "log-comment" (text comment)
+            tagc "pre" "sqlentry" (text q)
+        ])
+    pres |> List.concat |> divc "crudo-log" |> renderPage
+
